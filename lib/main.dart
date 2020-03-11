185aea323aec90565f38286ae0f7e66b1ef3ed66
nouh ben kadi <benkadi.nouh@icloud.com>
2020-03-11 22:21:40 +0100

    multiple sounds are working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -5,10 +6,66 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void _playNote(int noteNumber) {
+      final AudioCache player = AudioCache();
+      player.play('note$noteNumber.wav');
+    }
+
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          elevation: 5.0,
+        ),
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FlatButton(
+                color: Colors.indigoAccent,
+                onPressed: () {
+                  _playNote(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.blueGrey,
+                onPressed: () {
+                  _playNote(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  _playNote(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  _playNote(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  _playNote(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.indigo,
+                onPressed: () {
+                  _playNote(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  _playNote(7);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
