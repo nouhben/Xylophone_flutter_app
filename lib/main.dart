501671050de3a0d686c7ce9be6b01e8d55530e58
nouh ben kadi <benkadi.nouh@icloud.com>
2020-03-12 13:42:55 +0100

    adding the app bar and using the orientation builder for making horizontal and vertical app

--- lib/main.dart
+++ lib/main.dart
@@ -20,25 +20,75 @@ class XylophoneApp extends StatelessWidget {
             child: Text(''),
           ),
         );
-
+    Column _portraitUI() => Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildKeyAndNote(color: Colors.redAccent, noteNumber: 1),
+            _buildKeyAndNote(color: Colors.white12, noteNumber: 2),
+            _buildKeyAndNote(color: Colors.green[800], noteNumber: 3),
+            _buildKeyAndNote(color: Colors.purple, noteNumber: 4),
+            _buildKeyAndNote(color: Colors.orange[900], noteNumber: 5),
+            _buildKeyAndNote(color: Colors.yellow[900], noteNumber: 6),
+            _buildKeyAndNote(color: Colors.blue[900], noteNumber: 7),
+          ],
+        );
+    Row _landscapeUI() => Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildKeyAndNote(color: Colors.redAccent, noteNumber: 1),
+            _buildKeyAndNote(color: Colors.white12, noteNumber: 2),
+            _buildKeyAndNote(color: Colors.green[800], noteNumber: 3),
+            _buildKeyAndNote(color: Colors.purple, noteNumber: 4),
+            _buildKeyAndNote(color: Colors.orange[900], noteNumber: 5),
+            _buildKeyAndNote(color: Colors.yellow[900], noteNumber: 6),
+            _buildKeyAndNote(color: Colors.blue[900], noteNumber: 7),
+          ],
+        );
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.list,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              print('see the list of my records');
+            },
+          ),
+          backgroundColor: Colors.black54,
+          titleSpacing: 20.0,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.fiber_manual_record,
+                  color: Colors.red[900],
+                ),
+                onPressed: () {
+                  print('start recording ...');
+                }),
+            IconButton(
+                icon: Icon(
+                  Icons.play_arrow,
+                  color: Colors.red[900],
+                ),
+                onPressed: () {
+                  print('play recording ...');
+                })
+          ],
+        ),
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              _buildKeyAndNote(color: Colors.redAccent, noteNumber: 1),
-              _buildKeyAndNote(color: Colors.white12, noteNumber: 2),
-              _buildKeyAndNote(color: Colors.green[800], noteNumber: 3),
-              _buildKeyAndNote(color: Colors.purple, noteNumber: 4),
-              _buildKeyAndNote(color: Colors.orange[900], noteNumber: 5),
-              _buildKeyAndNote(color: Colors.yellow[900], noteNumber: 6),
-              _buildKeyAndNote(color: Colors.blue[900], noteNumber: 7),
-            ],
+          child: OrientationBuilder(
+            builder: (context, orientation) =>
+                orientation == Orientation.portrait
+                    ? _portraitUI()
+                    : _landscapeUI(),
           ),
         ),
       ),
     );
   }
 }
+/*
+* * */
